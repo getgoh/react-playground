56a7c557d32d30c3f3a50d0961888328b9b93e47
GET-PC-MSI\Get <agohca@gmail.com>
2019-04-02 11:37:01 -0400

    Updates

---
+++ precache-manifest.3d6e4998b6d5b622d054819bb84d1423.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-playground/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "409da0d949041dee4361",
+    "url": "/react-playground/static/js/runtime~main.6f2a4546.js"
+  },
+  {
+    "revision": "e52607b677af897ee5fb",
+    "url": "/react-playground/static/js/main.9ef71bd0.chunk.js"
+  },
+  {
+    "revision": "b6aafa07ca16cd706e06",
+    "url": "/react-playground/static/js/2.2752fb64.chunk.js"
+  },
+  {
+    "revision": "e52607b677af897ee5fb",
+    "url": "/react-playground/static/css/main.2703f83a.chunk.css"
+  },
+  {
+    "revision": "58c9424481314106b255a38031440ab7",
+    "url": "/react-playground/index.html"
+  }
+];
